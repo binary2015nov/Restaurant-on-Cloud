bfcb29179da418b39342173a70fb1d850b6e5c7d
binary2015nov <binary2015nov@outlook.com>
2024-07-31 10:55:09 +0800

    add history of orders page

---
+++ his_orders.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from session_builder import session
+
+sql = f'''
+SELECT A.ORDER_ID, A.ORDER_DETAILS_ID, DISH_NAME, ROUND(DISH_PRICE, 2) AS DISH_PRICE, ROUND(DISH_AMOUNT) AS DISH_AMOUNT, ORDER_TIME, DISH_PRICE * DISH_AMOUNT AS TOTAL_COST 
+FROM ORDERING_SYSTEM.CORE.DISH_ORDER A 
+  INNER JOIN ORDERING_SYSTEM.CORE.ORDER_DETAILS B 
+    ON A.ORDER_DETAILS_ID = B.ORDER_DETAILS_ID
+  INNER JOIN ORDERING_SYSTEM.CORE.MENU C 
+    ON B.DISH_ID  = C.DISH_ID
+WHERE A.USERNAME = '{st.session_state.username}'
+'''
+
+df = session.sql(sql).to_pandas()
+df_grouped = df.groupby('ORDER_ID')
+
+st.title(st.session_state.username + f"'s Orders history - {df_grouped.ngroups}:")
+
+for order_id, order_details in df_grouped:
+    st.subheader(f"Order ID: {order_id}\tTotal Cost: ${order_details['TOTAL_COST'].sum():.2f}") 
+    st.dataframe(order_details[['DISH_NAME', 'DISH_PRICE', 'DISH_AMOUNT', 'ORDER_TIME']], hide_index=True)
+
--- main_page.py
+++ main_page.py
@@ -10,7 +10,9 @@ def login():
     username = form.text_input("Username", value='')
     pwd = form.text_input("Password", value='', type="password")
     if form.form_submit_button("Login"):
-        if username == "admin" and pwd=="admin":
+        if username and pwd=="admin":
+            st.session_state.username = username
+            print(st.session_state.username)
             st.session_state.logged_in = True
             st.rerun()
         else: 
@@ -24,13 +26,14 @@ login_page = st.Page(login, title="Login", icon=":material/login:")
 logout_page = st.Page(logout, title="Logout", icon=":material/logout:")
 
 order_pg = st.Page("order_system.py", title="Start your order", icon="✅")
+his_orders = st.Page("his_orders.py", title="History orders", icon="📝")
 his_trend = st.Page("his_trend.py", title="History trend", icon="📈")
 ingre_profits = st.Page("ingre_profits.py", title="Ingredient profit", icon="🌍")
 ingre_sales = st.Page("ingre_sales.py", title="Ingredient sales", icon="📊")
 ivt_monitor = st.Page("inventory_monitor.py", title="Inventory monitor", icon="🚨")
 
 if st.session_state.logged_in:
-    pg = st.navigation([order_pg, his_trend,ingre_profits,ingre_sales,ivt_monitor, logout_page])
+    pg = st.navigation([order_pg, his_orders, his_trend,ingre_profits,ingre_sales,ivt_monitor, logout_page])
 else:
     pg = st.navigation([login_page])
 
